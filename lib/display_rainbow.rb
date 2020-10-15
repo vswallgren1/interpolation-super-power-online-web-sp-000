7b0edbb8d0813e39cefec44d8b969ae130d42c09
Vince W <vincewallgren@me.com>
2020-10-15 03:07:30 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
 def display_rainbow(colors)
-  puts ["R: red" "O: orange", "Y: yellow", "G: green", "B: blue", "I: indigo", "V: violet"]
+  puts "R: red" "O: orange", "Y: yellow", "G: green", "B: blue", "I: indigo", "V: violet"
 end
